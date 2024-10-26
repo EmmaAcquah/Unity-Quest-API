91bfceb6ed864133d0705cd077f14de208f562dc
Emma Acquah <emmaacquah@gmail.com>
2024-10-26 17:15:08 +0100

    Spring Boot Project Setup

---
+++ src/main/java/com/example/unity_quest/unity_quest_backend/UnityQuestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.unity_quest.unity_quest_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnityQuestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnityQuestBackendApplication.class, args);
+	}
+
+}
--- src/main/java/com/example/unity_quest/unity_quest_backend/controller/ChallengeController.java
+++
@@ -1,4 +0,0 @@
-package com.example.unity_quest.unity_quest_backend.controller;
-
-public class ChallengeController {
-}
