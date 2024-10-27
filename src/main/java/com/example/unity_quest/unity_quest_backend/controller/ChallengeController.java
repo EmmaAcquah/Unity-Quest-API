596257050290af8e78f18cfd884756db52a6bf96
Emma Acquah <emmaacquah@gmail.com>
2024-10-27 08:01:40 +0000

    Add API Controller endpoints (list all challenges, findById, save).

--- src/main/java/com/example/unity_quest/unity_quest_backend/controller/ChallengeController.java
+++ src/main/java/com/example/unity_quest/unity_quest_backend/controller/ChallengeController.java
@@ -1,14 +1,38 @@
 package com.example.unity_quest.unity_quest_backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.unity_quest.unity_quest_backend.model.Challenge;
+import com.example.unity_quest.unity_quest_backend.service.ChallengeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ChallengeController {
 
+    @Autowired
+    ChallengeService challengeService;
+
     @GetMapping("/")
     public String testHomepage() {
-        return "Hello World!";
+        return "Welcome to Unity Quest!";
+    }
+
+    @GetMapping("/challenges")
+    public List<Challenge> getAll() {
+        return challengeService.getAllChallenges();
+    }
+
+    @GetMapping("challenges/{id}")
+    public Challenge findById(@PathVariable Long id) {
+        return challengeService.findChallengeById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED) // 201
+    @PostMapping("save")
+    public Challenge save(@RequestBody Challenge challenge) {
+        return challengeService.saveChallenge(challenge);
     }
 
 }
